2896d973cb9e4015b10d07697f151c32f6fd98c5
yefan <13259459957@163.com>
2017-04-09 12:28:24 +0800

    监狱登录页

---
+++ work/prison/js/login.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/4/7.
+ */
+var app = angular.module('login', []);
+app.controller('loginCtrl', function($scope) {
+    var date = new Date();
+    $scope.time = date;
+    var hours = date.getHours();
+    var minute = date.getMinutes();
+    $scope.test = 111;
+});
